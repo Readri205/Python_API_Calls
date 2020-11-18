487ab835cf7bce122c6062dccc08c37e2bfe984c
Readri205 <65308512+Readri205@users.noreply.github.com>
2020-11-18 18:59:23 +0000

    Update

--- trefle_io_plants.py
+++ trefle_io_plants.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 #    return "Hello World ... again!"
 
 
-ENDPOINT = "https://trefle.io/api/v1/plants/search?"
+ENDPOINT = "https://trefle.io/api/v1/plants/search?token="
 YOUR_TREFLE_TOKEN = os.environ.get("YOUR_TREFLE_TOKEN")
 PAGE_NUMBER = "&page=20"
 
@@ -23,13 +23,13 @@ PAGE_NUMBER = "&page=20"
 ENDPOINT_SPECIES = "https://trefle.io/api/v1/species?"
 FILTER = "&filter[common_name]=coconut%20palm"
 STRG = "&q="
-SEARCH = "shoe black"
+SEARCH = "lily"
 
 r = requests.get(
-    f"{ENDPOINT}token={YOUR_TREFLE_TOKEN}{STRG}{SEARCH}")
+    f"{ENDPOINT}{YOUR_TREFLE_TOKEN}{STRG}{SEARCH}")
 
 species_filter = requests.get(
-    f"{ENDPOINT_SPECIES}token={YOUR_TREFLE_TOKEN}{FILTER}")
+    f"{ENDPOINT_SPECIES}{YOUR_TREFLE_TOKEN}{FILTER}")
 
 plants = r.json()
 
@@ -47,8 +47,14 @@ for plant in plants['data']:
     family_common_name = plant['family_common_name']
     image = plant['image_url']
     links = plant['links']
-    print(f"Plant ID: {plant_id}\tName: {name}\tFamily:{family}\tFamily Common Name:{family_common_name}\tImage: {image}\n")
+    species = links['self']
+    print(f"Plant ID: {plant_id}\tName: {name}\tFamily:{family}\tFamily Common Name:{family_common_name}\tImage: {image}")
+    print(f"{species}\n")
 
+links = plants['links']
+current = links['self']
+first = links['first']
+print(f"{current}, {first}")
 
 # print(species_filter)
 
